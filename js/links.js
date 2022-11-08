84a25b108d1ccf3577a3934dbff0d1f2adfb3a97
jedihilario <hyanezbertola@gmail.com>
2022-11-08 20:42:36 -0300

    Updated design

--- js/links.js
+++ js/links.js
@@ -1,48 +1,104 @@
 const links = [
     {
-        link: 'https://i.pinimg.com/originals/f7/ca/00/f7ca0065ebb40873f051bf45b91370c3.png',
-        title: 'Cafe Cafe',
-        category: 'Matematica',
-        tags: ['Calculo', 'El pepe', 'Prueba']
+        title: "Introduccion a las matematicas",
+        link: "https://n9.cl/matematicasproa",
+        category: "matematica",
+        tags: ["numeros reales", "operaciones basicas", "expresiones algebraicas", "polinomios", "regla de ruffini", "factorizacion", "divisibilidad", "racionalizacion", "ecuaciones de primer grado", "funcion lineal", "sistema de ecuaciones lineales", "ecuaciones de segundo grado", "funcion cuadratica", "ecuacion exponencial", "funcion exponencial", "funcion logaritmica", ],
+    },    
+    {
+        title: "Numeros y operaciones arimeticas",
+        link: "https://n9.cl/matematicasproa3",
+        category: "matematica",
+        tags: ["numeros naturales", "numeros enteros", "numeros racionales", "propiedad conmutativa", "propiedad asociativa", "propiedad distributiva", "suma", "resta", "numeros reales", "funcion lineal", "ecuacion de primer grado", "ecuacion de segundo grado"], 
+    },    
+    {
+        title: "Aritmetica basica",
+        link: "https://n9.cl/matematicasproa4",
+        category: "matematica",
+        tags: ["numeros naturales", "numeros enteros", "numeros racionales", "numeros irracionales", "numeros reales", "suma", "resta", "multiplicacion", "asociativa", "conmutativa", "distributiva", "maximo comun multiplo", "minimo comun multiplo", "divisibilidad"], 
+    },    
+    {
+        title: "Matematica polimodal",
+        link: "https://n9.cl/matematicasproa6",
+        category: "matematica",
+        tags: ["numeros reales", "matrices", "expresiones algebraicas", "funciones", "ecuaciones", "numeros enteros", "numeros naturales", "numeros racionales", "recta real", "intervalo", "suma", "resta", "potenciacion", "division", "raiz", "radicacion", "funcion lineal", "dominio", "imagen", "inecuacion", "ecuaciones de primer grado", "ecuaciones de segundo grado"], 
     },
     {
-        link: 'https://i.pinimg.com/originals/f7/ca/00/f7ca0065ebb40873f051bf45b91370c3.png',
-        title: 'Jhon',
-        category: 'Lengua'
+        title: "Algebra y geometria",
+        link: "https://n9.cl/matematicasproa7",
+        category: "matematica",
+        tags: ["matrices", "algebra", "geometria", "ecuacion lineal", "vectores"], 
     },
     {
-        link: 'https://i.pinimg.com/originals/f7/ca/00/f7ca0065ebb40873f051bf45b91370c3.png',
-        title: 'Tilin',
-        category: 'Historia'
+        title: "Matematicas generales",
+        link: "https://n9.cl/matematicasproa8",
+        category: "matematica",
+        tags: ["ecuaciones", "ecuacion vectorial", "rectas sectantes", "mediatriz", "numeros reales", "polinomios", "regla de ruffini", "logaritmos", "funcion derivada", "funcion raiz cuadrada", "limites de funciones polinomicas", "calculo de limites", "vectores", "bisectriz", "graficos", "tablas"], 
     },
     {
-        link: 'https://i.pinimg.com/originals/f7/ca/00/f7ca0065ebb40873f051bf45b91370c3.png',
-        title: 'Ete Sech',
-        category: 'Historia'
+        title: "Prehistoria hasta la actualidad",
+        link: "https://n9.cl/historiaproa",
+        category: "historia",
+        tags: ["historia argentina", "culturas aborigenes", "presidencias fundantes"],
     },
     {
-        link: 'https://i.pinimg.com/originals/f7/ca/00/f7ca0065ebb40873f051bf45b91370c3.png',
-        title: 'Potatzio',
-        category: 'Matematica'
+        title: "Historia medieval",
+        link: "https://n9.cl/historiaproa2",
+        category: "historia",
+        tags: ["epoca medieval", "edad media", "arte medieval", "periodo carolingio y otoniano"],
     },
     {
-        link: 'https://i.pinimg.com/originals/f7/ca/00/f7ca0065ebb40873f051bf45b91370c3.png',
-        title: 'Pachani',
-        category: 'Programacion'
+        title: "Historia mundial contemporanea",
+        link: "https://n9.cl/historiaproa3",
+        category: "historia",
+        tags: ["sociedades burguesas","sociedad liberal","sociedad capitalista", "revolucion industrial", "revolucion francesa", "liberalismo", "segunda revolucion industrial", "imperialismo", "division internacional del trabajo", "revolucion rusa", "fascismo italiano", "nazismo", "primera guerra mundial", "segunda guerra mundial", "guerra civil española", "guerra fria", "mundo bipolar", "revolucion cubana", "neoliberalismo", "estado sovietico"],
     },
     {
-        link: 'https://i.pinimg.com/originals/f7/ca/00/f7ca0065ebb40873f051bf45b91370c3.png',
-        title: 'Pollo',
-        category: 'Programacion'
+        title: "Manual historia argentina",
+        link: "https://n9.cl/historiaproa4",
+        category: "historia",
+        tags: ["iluminismo", "invasiones inglesas", "revolucion de mayo", "cabildo abierto", "unitarismo", "federalismo", "liga unitario", "pacto federal", ""],
+    },    
+    {
+        title: "Historia del mundo contemporaneo",
+        link: "https://n9.cl/historiaproa5",
+        category: "historia",
+        tags: ["El Imperialismo, La Primera Guerra Mundial y la Revolución Rusa, Período Entreguerras en el ámbito capitalista, La Segunda Guerra Mundial y el Holocausto"],
     },
     {
-        link: 'https://i.pinimg.com/originals/f7/ca/00/f7ca0065ebb40873f051bf45b91370c3.png',
-        title: 'George',
-        category: 'Biologia'
+        title: "Historia conectar igualdad",
+        link: "https://n9.cl/historiaproa6",
+        category: "historia",
+        tags: ["modelo agroexportador, Globalización y Estados nacionales, Civilizaciones antiguas latinoamericanas"],
     },
     {
-        link: 'https://i.pinimg.com/originals/f7/ca/00/f7ca0065ebb40873f051bf45b91370c3.png',
-        title: 'Esti',
-        category: 'Lengua'
+        title: "Historia normal",
+        link: "https://n9.cl/historia7",
+        category: "historia",
+        tags: ["argentina entre de 1810 y 1852"],
+    },
+    {
+        title: "Historia contemporanea",
+        link: "https://n9.cl/historiaproa8",
+        category: "historia",
+        tags: ["politicas y dirigentes del pan, juares celma, UCN y UCR"],
+    },
+    {
+        title: "Historia epoca de rosas",
+        link: "https://n9.cl/historiaproa9",
+        category: "historia",
+        tags: ["epoca de rosas, la decada del 80, argentina a principios del siglo 21 "],
+    },
+    {
+        title: "Historia entre guerras",
+        link: "https://n9.cl/georgeproa10",
+        category: "historia",
+        tags: ["capitalista y liberalista, fin de la guerra fria"],
+    },
+    {
+        title: "Historia 1852 a 1890",
+        link: "https://n9.cl/tomiproa11",
+        category: "historia",
+        tags: ["Urquiza, el vencedor, La revolución"],
     }
 ];
